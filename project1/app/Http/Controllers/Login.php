6a87d7808ee3d5322d576c06ae3f3ed80ebc33d8
ameen1214 <103337107+ameen1214@users.noreply.github.com>
2023-03-04 14:08:38 -0500

    test

--- project1/app/Http/Controllers/Login.php
+++ project1/app/Http/Controllers/Login.php
@@ -3,13 +3,90 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\Models\User;
+use Validator;
 class Login extends Controller
 {
     public function login(Request $request){
-    	if(auth->attempt(['email'=>$request->input('email'), 'password'=>$request->input('password') ])){
+    	$validator=Validator::make($request->all(),[
+    		'email' => 'required|max:191|string',
+    		'password' => 'required|max:191|string'
+    	]);
+    	if ($validator->fails()) {
+    		return $validator->errors();
+    	}
+        if(auth()->attempt([
+        	'email'=>$request->input('email'),
+        	'password'=>$request->input('password')
+        ])){
+        	$user=auth()->user();
+        	$user->api_token=Str::random(60);
+        	$user->save();
+        	return "ok";
+        }
+        return "no";
+    }
+
+    public function logout(Request $request){
+    	$validator=Validator::make($request->all(),[
+    		'email' => 'required|max:191|string',
+    		'password' => 'required|max:191|string'
+    	]);
+    	if ($validator->fails()) {
+    		return $validator->errors();
+    	}
+        if(auth()->attempt([
+        	'email'=>$request->input('email'),
+        	'password'=>$request->input('password')
+        ])){
+        	$user=auth()->user();
+        	$user->api_token="";
+        	$user->save();
+        	return "ok";
+        }
+        return "no";
+    }
+
+    public function register(Request $request) {		
+		$validator=Validator::make($request->all(),[
+    		'name' => 'required|max:191|string',
+    		'email' => 'required|max:191|string|unique:users',
+    		'password' => 'required|max:191|string'
+    	]);
+    	if ($validator->fails()) {
+    		return $validator->errors();
+    	}
+    	else{
+    		$data=User::create([
+    			'name'=>$request->name,
+    			'email'=>$request->email,
+    			'password'=>Hash::make($request->password),
+    			'api_token' =>""
+    		]);
     		return "ok";
     	}
-    	return "no";
-    }
+		
+	}
+
+	public function updatePassword(Request $request){
+		$validator=Validator::make($request->all(),[
+    		'email' => 'required|max:191|string',
+    		'password' => 'required|max:191|string|unique:users',
+    		'password2' => 'required|max:191|string'
+    	]);
+    	if ($validator->fails()) {
+    		return $validator->errors();
+    	}
+    	else{
+    		if(auth()->attempt(['email'=>$request->input('email'),
+        		'password'=>$request->input('password')]))
+    		{
+        		User::where('email',$request->input('email'))->update(['password'=>Hash::make($request->password2)]);
+        		return "updated";
+        	}
+        	return "could not update";
+    	}
+	}
 }
--- project1/routes/api.php
+++ project1/routes/api.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::post('register',[App\Http\Controllers\Register::class, 'register']);
+//
+Route::post('register',[App\Http\Controllers\Login::class, 'register']);
 Route::post('login',[App\Http\Controllers\Login::class, 'login']);
+Route::post('logout',[App\Http\Controllers\Login::class, 'logout']);
+Route::post('updatePassword',[App\Http\Controllers\Login::class, 'updatePassword']);
+Route::get('/', function () {
+    return "ok";
+});
